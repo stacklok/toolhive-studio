ccc85ca378ca4904aa5bbd51580fbaea8843fc6a
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-05-30 15:20:10 +0100

    chore: tidy up

--- src/app/routes/registry.tsx
+++ src/app/routes/registry.tsx
@@ -7,27 +7,10 @@ export const Route = createFileRoute("/registry")({
 });
 
 function RouteComponent() {
-  // const [isRunWithCommandOpen, setIsRunWithCommandOpen] = useState(false);
-
-  // const { mutateAsync } = useToastMutation(postApiV1BetaServersMutation());
-
   return (
     <>
       <div className="flex items-center mb-6">
         <h1 className="font-semibold text-3xl">Registry</h1>
-        {/* <DropdownMenuRunMcpServer
-          openRunCommandDialog={() => setIsRunWithCommandOpen(true)}
-          className="ml-auto"
-        /> */}
-        {/* <DialogFormRunMcpServerWithCommand
-          isOpen={isRunWithCommandOpen}
-          onOpenChange={setIsRunWithCommandOpen}
-          onSubmit={(data) => {
-            mutateAsync({
-              body: data,
-            });
-          }}
-        /> */}
       </div>
       <GridCardsMcpRegistryItems mcpRegistryItems={MCP_REGISTRY} />
     </>
