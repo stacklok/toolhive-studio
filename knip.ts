a667d25f0c2f1011942e96cf136d7225e4896a9d
Daniel Kantor <kd@stacklok.com>
2025-06-03 10:59:13 +0200

    re-add some removed files

---
+++ knip.ts
@@ -0,0 +1,17 @@
+export default {
+  entry: ["src/main.tsx", "src/index.css"],
+  project: ["src/**"],
+  ignore: [
+    "src/app/route-tree.gen.ts",
+    "src/common/api/generated/**",
+    "src/common/hooks/use-servers.ts",
+    "src/common/lib/utils.ts",
+    "src/common/components/ui/**",
+  ],
+  ignoreDependencies: ["@testing-library/user-event"],
+  compilers: {
+    // this is needed to support css entry files
+    // see: https://knip.dev/features/compilers#css
+    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join("\n"),
+  },
+};
