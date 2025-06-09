c045574c25329c550eebc343f9c691402d8bf24d
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-09 15:42:16 +0200

    chore: add csp

---
+++ main/src/csp.ts
@@ -0,0 +1,20 @@
+const cspMap = {
+  'default-src': "'self'",
+  'script-src': "'self'",
+  'style-src': "'self' 'unsafe-inline'",
+  'img-src': "'self' data: blob:",
+  'font-src': "'self' data:",
+  'connect-src': "'self' http://localhost:*",
+  'frame-src': "'none'",
+  'object-src': "'none'",
+  'base-uri': "'self'",
+  'form-action': "'self'",
+  'frame-ancestors': "'none'",
+  'manifest-src': "'self'",
+  'media-src': "'self' blob: data:",
+  'worker-src': "'self'",
+}
+
+export const cspString = Object.entries(cspMap)
+  .map(([key, value]) => `${key} ${value}`)
+  .join('; ')
