b29b9aeb399d1e7d0e6954401454dc1d7e4975bf
Daniel Kantor <kd@stacklok.com>
2025-06-11 14:59:53 +0200

    clean up directory structure

--- renderer/src/common/test/render-route.tsx
+++ renderer/src/common/test/render-route.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import { render } from '@testing-library/react'
 import { createTestRouter } from './create-test-router'
-import { ConfirmProvider } from '@/common/contexts/confirm-context'
+import { ConfirmProvider } from '../contexts/confirm/provider'
 
 // NOTE: This is used only to infer a type for the router
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
