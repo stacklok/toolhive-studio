4d3d8efab728d759ffaf1b46a7b26a3ff82f7ee5
Samuele Verzi <samuele.v@me.com>
2025-06-12 09:14:24 +0200

    fix(config): fix vite renderer configuration issues

--- renderer/vite.renderer.config.ts
+++ renderer/vite.renderer.config.ts
@@ -2,38 +2,40 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
+import autoprefixer from 'autoprefixer'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/postcss'
 
-export default defineConfig(async () => {
-  const { default: tailwindcss } = await import('@tailwindcss/vite')
-  const { TanStackRouterVite } = await import('@tanstack/router-plugin/vite')
-
-  return {
-    root: __dirname,
-    build: {
-      sourcemap: true, // Required for Sentry sourcemaps
-      outDir: path.resolve(__dirname, '../.vite/renderer/main_window'),
+export default defineConfig({
+  root: __dirname,
+  build: {
+    sourcemap: true, // Required for Sentry sourcemaps
+    outDir: path.resolve(__dirname, '../.vite/renderer/main_window'),
+  },
+  plugins: [
+    TanStackRouterVite({
+      target: 'react',
+      autoCodeSplitting: true,
+      routesDirectory: path.resolve(__dirname, './src/routes'),
+      generatedRouteTree: path.resolve(__dirname, './src/route-tree.gen.ts'),
+      quoteStyle: 'double',
+      routeFileIgnorePattern: '__tests__',
+    }),
+    react(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN, // NOTE: This should be available only during CI
+      org: process.env.SENTRY_ORG, // NOTE: This should be available only during CI
+      project: process.env.SENTRY_PROJECT, // NOTE: This should be available only during CI
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
-    plugins: [
-      TanStackRouterVite({
-        target: 'react',
-        autoCodeSplitting: true,
-        routesDirectory: path.resolve(__dirname, './src/routes'),
-        generatedRouteTree: path.resolve(__dirname, './src/route-tree.gen.ts'),
-        quoteStyle: 'double',
-        routeFileIgnorePattern: '__tests__',
-      }),
-      react(),
-      tailwindcss(), // now loaded via dynamic import → no require() conflict
-      sentryVitePlugin({
-        authToken: process.env.SENTRY_AUTH_TOKEN, // NOTE: This should be available only during CI
-        org: process.env.SENTRY_ORG, // NOTE: This should be available only during CI
-        project: process.env.SENTRY_PROJECT, // NOTE: This should be available only during CI
-      }),
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
 })
