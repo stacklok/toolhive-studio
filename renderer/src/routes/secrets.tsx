a5dab3dce8d4831613b6ee1f8ed149ab68ed8287
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-17 15:41:09 +0200

    refactor: remove mocked data

--- renderer/src/routes/secrets.tsx
+++ renderer/src/routes/secrets.tsx
@@ -4,15 +4,7 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/secrets')({
   component: Secrets,
 })
-const SECRETS = {
-  keys: [
-    { key: 'Github' },
-    { key: 'Grafana' },
-    { key: 'Slack' },
-    { key: 'Jira' },
-  ],
-}
 
 function Secrets() {
-  return <SecretsTable secrets={SECRETS.keys} />
+  return <SecretsTable secrets={[]} />
 }
