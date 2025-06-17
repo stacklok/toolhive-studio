367c6bbba7902b85613aa4943de42feb98b68ad1
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-17 15:30:59 +0200

    feat: add secrets mock

--- renderer/src/routes/secrets.tsx
+++ renderer/src/routes/secrets.tsx
@@ -1,9 +1,18 @@
+import { SecretsTable } from '@/features/secrets/components/secrets-table'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/secrets')({
-  component: RouteComponent,
+  component: Secrets,
 })
+const SECRETS = {
+  keys: [
+    { key: 'Github' },
+    { key: 'Grafana' },
+    { key: 'Slack' },
+    { key: 'Jira' },
+  ],
+}
 
-function RouteComponent() {
-  return <div>Hello "/secrets"!</div>
+function Secrets() {
+  return <SecretsTable secrets={SECRETS.keys} />
 }
