daa67dce6a416512d9f4e1c2ba6d25bba9b6b28c
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-19 12:21:34 +0200

    fix(clients): use proper naming for client type

--- renderer/src/features/clients/components/card-client.tsx
+++ renderer/src/features/clients/components/card-client.tsx
@@ -7,12 +7,24 @@ import {
 } from '@/common/components/ui/card'
 import { Switch } from '@/common/components/ui/switch'
 
+// There is an issue with openAPI generator in BE, similar issue https://github.com/stacklok/toolhive/issues/780
+const CLIENT_TYPE_LABEL_MAP = {
+  'roo-code': 'Roo Code',
+  cline: 'Cline',
+  'vscode-insider': 'VS CodeI nsider',
+  vscode: 'VS Code',
+  cursor: 'Cursor',
+  'claude-code': 'Claude Code',
+} as const
+
 export function CardClient({ client }: { client: ClientMcpClientStatus }) {
   return (
     <Card className="gap-3 py-5">
       <CardHeader>
         <CardTitle className="flex items-center text-xl">
-          {client.client_type}
+          {CLIENT_TYPE_LABEL_MAP[
+            client.client_type as keyof typeof CLIENT_TYPE_LABEL_MAP
+          ] ?? client.client_type}
         </CardTitle>
       </CardHeader>
       <CardContent className="flex items-center gap-2">
