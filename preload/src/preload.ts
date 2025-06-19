b972de8266fb7dd60676771c5c2c20a59eb358a3
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-06-19 17:03:47 +0100

    chore: temp disable generateApiClient during build

--- preload/src/preload.ts
+++ preload/src/preload.ts
@@ -16,7 +16,8 @@ contextBridge.exposeInMainWorld('electronAPI', {
   quitApp: () => ipcRenderer.invoke('quit-app'),
 
   // ToolHive port
-  getToolhivePort: () => ipcRenderer.invoke('get-toolhive-port'),
+  // getToolhivePort: () => ipcRenderer.invoke('get-toolhive-port'),
+  getToolhivePort: () => 8080,
 
   // Theme management
   darkMode: {
