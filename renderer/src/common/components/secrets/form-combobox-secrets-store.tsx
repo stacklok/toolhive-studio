e8537098f5224975a969038fb8a6b71054b27d86
Alex McGovern <58784948+alex-mcgovern@users.noreply.github.com>
2025-06-20 15:18:11 +0100

    feat: fetch secrets in combobox (#248)

--- renderer/src/common/components/secrets/form-combobox-secrets-store.tsx
+++ renderer/src/common/components/secrets/form-combobox-secrets-store.tsx
@@ -1,4 +1,4 @@
-import { Check, ChevronDown } from 'lucide-react'
+import { Check, ChevronDown, RefreshCwIcon } from 'lucide-react'
 import { Button } from '../ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 import type { FieldValues, Path, UseFormReturn } from 'react-hook-form'
@@ -12,24 +12,9 @@ import {
   CommandList,
 } from '../ui/command'
 import { cn } from '@/common/lib/utils'
-
-const MOCK_SECRETS: { key: string }[] = [
-  {
-    key: 'CONFLUENCE_API_TOKEN',
-  },
-  {
-    key: 'JIRA_API_TOKEN',
-  },
-  {
-    key: 'GITHUB_ACCESS_TOKEN',
-  },
-  {
-    key: 'GITHUB_WEBHOOK_SECRET',
-  },
-  {
-    key: 'AWS_ACCESS_KEY_ID',
-  },
-]
+import { useQuery } from '@tanstack/react-query'
+import { getApiV1BetaSecretsDefaultKeysOptions } from '@/common/api/generated/@tanstack/react-query.gen'
+import { useEffect, useState } from 'react'
 
 type ConstrainedFieldValues = FieldValues & {
   secrets: {
@@ -42,6 +27,31 @@ type ConstrainedFieldValues = FieldValues & {
 }
 
 /**
+ * A custom hook that resets a boolean value after a specified delay.
+ */
+const useDelayedReset = (initialValue = false, delayMs = 1000) => {
+  const [value, setValue] = useState<boolean>(initialValue)
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined
+
+    if (value) {
+      timeoutId = setTimeout(() => {
+        setValue(false)
+      }, delayMs)
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [value, delayMs])
+
+  return [value, setValue] as const
+}
+
+/**
  * A combobox for selecting a secret from the secret store.
  * NOTE: This component expects that the form has a field named `secrets` which
  * is an array of objects with a `key` & `value` property.
@@ -49,6 +59,15 @@ type ConstrainedFieldValues = FieldValues & {
 export function FormComboboxSecretStore<
   T extends ConstrainedFieldValues = ConstrainedFieldValues,
 >({ form, name }: { form: UseFormReturn<T>; name: Path<T> }) {
+  const { data, refetch } = useQuery(getApiV1BetaSecretsDefaultKeysOptions())
+
+  const [isAnimating, setIsAnimating] = useDelayedReset(false, 500)
+
+  function handleRefresh() {
+    refetch()
+    setIsAnimating(true)
+  }
+
   return (
     <FormField
       control={form.control}
@@ -74,30 +93,59 @@ export function FormComboboxSecretStore<
               side="bottom"
             >
               <Command>
-                <CommandInput placeholder="Search secrets..." className="h-9" />
+                <div className="grid w-full grid-cols-[auto_calc(var(--spacing)_*_9)] items-end">
+                  <CommandInput
+                    placeholder="Search secrets..."
+                    className="h-9"
+                  />
+                  <div
+                    className="ml-auto flex size-9 shrink-0 grow-0 items-center justify-center self-end
+                      border-b"
+                  >
+                    <Button
+                      className={cn('text-muted-foreground size-7')}
+                      variant="ghost"
+                      type="button"
+                      onClick={() => handleRefresh()}
+                    >
+                      <RefreshCwIcon
+                        className={isAnimating ? 'animate-spin' : ''}
+                      />
+                    </Button>
+                  </div>
+                </div>
                 <CommandList>
                   <CommandEmpty>No secrets found</CommandEmpty>
                   <CommandGroup>
-                    {MOCK_SECRETS.map((secret) => (
-                      <CommandItem
-                        key={secret.key}
-                        value={secret.key}
-                        className="font-mono"
-                        onSelect={(value) => {
-                          field.onChange({ secret: value, isFromStore: true })
-                        }}
-                      >
-                        {secret.key}
-                        <Check
-                          className={cn(
-                            'ml-auto',
-                            field.value.secret === secret.key
-                              ? 'opacity-100'
-                              : 'opacity-0'
-                          )}
-                        />
-                      </CommandItem>
-                    ))}
+                    {data?.keys?.[0]
+                      ? data.keys
+                          .sort((a, b) =>
+                            (a.key ?? '')?.localeCompare(b.key ?? '')
+                          )
+                          .map((secret) => (
+                            <CommandItem
+                              key={secret.key}
+                              value={secret.key}
+                              className="font-mono"
+                              onSelect={(value) => {
+                                field.onChange({
+                                  secret: value,
+                                  isFromStore: true,
+                                })
+                              }}
+                            >
+                              {secret.key}
+                              <Check
+                                className={cn(
+                                  'ml-auto',
+                                  field.value.secret === secret.key
+                                    ? 'opacity-100'
+                                    : 'opacity-0'
+                                )}
+                              />
+                            </CommandItem>
+                          ))
+                      : null}
                   </CommandGroup>
                 </CommandList>
               </Command>
