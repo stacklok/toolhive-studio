8bf3e6e0626a20c11eb7265eb1062474eb4ec61d
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-23 17:53:17 +0200

    chore: format

--- renderer/src/features/secrets/hooks/use-mutation-update-secret.ts
+++ renderer/src/features/secrets/hooks/use-mutation-update-secret.ts
@@ -1,6 +1,9 @@
-import { getApiV1BetaSecretsDefaultKeysQueryKey, putApiV1BetaSecretsDefaultKeysByKeyMutation } from "@/common/api/generated/@tanstack/react-query.gen";
-import { useToastMutation } from "@/common/hooks/use-toast-mutation";
-import { useQueryClient } from "@tanstack/react-query";
+import {
+  getApiV1BetaSecretsDefaultKeysQueryKey,
+  putApiV1BetaSecretsDefaultKeysByKeyMutation,
+} from '@/common/api/generated/@tanstack/react-query.gen'
+import { useToastMutation } from '@/common/hooks/use-toast-mutation'
+import { useQueryClient } from '@tanstack/react-query'
 
 export function useMutationUpdateSecret(secretKey: string) {
   const queryClient = useQueryClient()
