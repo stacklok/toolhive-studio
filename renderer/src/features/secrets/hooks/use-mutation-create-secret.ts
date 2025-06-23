1224a8199e6bf0ef9f6ffa16f1b9570b82d7d254
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-23 15:42:56 +0200

    feat(secrets): create custom hooks mutation

---
+++ renderer/src/features/secrets/hooks/use-mutation-create-secret.ts
@@ -0,0 +1,18 @@
+import {
+  getApiV1BetaSecretsDefaultKeysQueryKey,
+  postApiV1BetaSecretsDefaultKeysMutation,
+} from '@/common/api/generated/@tanstack/react-query.gen'
+import { useToastMutation } from '@/common/hooks/use-toast-mutation'
+import { useQueryClient } from '@tanstack/react-query'
+
+export function useMutationCerateSecret() {
+  const queryClient = useQueryClient()
+  return useToastMutation({
+    ...postApiV1BetaSecretsDefaultKeysMutation(),
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: getApiV1BetaSecretsDefaultKeysQueryKey(),
+      })
+    },
+  })
+}
---
+++ renderer/src/features/secrets/hooks/use-mutation-delete-secret.ts
@@ -0,0 +1,18 @@
+import {
+  deleteApiV1BetaSecretsDefaultKeysByKeyMutation,
+  getApiV1BetaSecretsDefaultKeysQueryKey,
+} from '@/common/api/generated/@tanstack/react-query.gen'
+import { useToastMutation } from '@/common/hooks/use-toast-mutation'
+import { useQueryClient } from '@tanstack/react-query'
+
+export function useMutationDeleteSecret() {
+  const queryClient = useQueryClient()
+  return useToastMutation({
+    ...deleteApiV1BetaSecretsDefaultKeysByKeyMutation(),
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: getApiV1BetaSecretsDefaultKeysQueryKey(),
+      })
+    },
+  })
+}
