fc1824138ffdf15f79e8b417623f3bd72c068c47
Giuseppe Scuglia <peppescg@gmail.com>
2025-06-23 18:39:53 +0200

    feat: handle connect/disconnect client (#276)

---
+++ renderer/src/features/clients/hooks/use-mutation-register-client.ts
@@ -0,0 +1,19 @@
+import {
+  getApiV1BetaDiscoveryClientsQueryKey,
+  postApiV1BetaClientsMutation,
+} from '@/common/api/generated/@tanstack/react-query.gen'
+import { useToastMutation } from '@/common/hooks/use-toast-mutation'
+import { useQueryClient } from '@tanstack/react-query'
+
+export function useMutationRegisterClient(clientName: string) {
+  const queryClient = useQueryClient()
+  return useToastMutation({
+    ...postApiV1BetaClientsMutation(),
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: getApiV1BetaDiscoveryClientsQueryKey(),
+      })
+    },
+    errorMsg: `Failed to connect ${clientName}`,
+  })
+}
