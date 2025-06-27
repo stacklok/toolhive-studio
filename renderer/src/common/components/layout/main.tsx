9fc8fc05834f70258bfe03b0b04b5a027d688bc0
Daniel Kantor <kd@stacklok.com>
2025-06-27 11:46:26 +0200

    refactor: allow "fit vertical height" layouts easily

--- renderer/src/common/components/layout/main.tsx
+++ renderer/src/common/components/layout/main.tsx
@@ -7,7 +7,7 @@ export function Main(props: { children: ReactNode; className?: string }) {
       {...props}
       className={twMerge(
         'flex min-h-0 w-full flex-1 flex-col',
-        'max-h-[calc(100dvh-calc(var(--spacing)_*_16))] overflow-y-auto',
+        'h-[calc(100dvh-calc(var(--spacing)_*_16))] overflow-y-auto',
         'px-3 py-4',
         props.className
       )}
