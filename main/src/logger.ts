03282b26aea76bddcd7f82c3b2f382e3c53f90a4
Giuseppe Scuglia <peppescg@gmail.com>
2025-07-04 13:30:03 +0200

    chore: create logger

---
+++ main/src/logger.ts
@@ -0,0 +1,29 @@
+import log from 'electron-log/main'
+import { app } from 'electron'
+import path from 'node:path'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+log.transports.file.resolvePathFn = () => {
+  const appName = app.getName()
+  console.log('appName', appName)
+  const logPath = app.getPath('logs')
+  return path.join(logPath, 'main.log')
+}
+
+log.transports.file.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}'
+log.transports.console.format = '{h}:{i}:{s}.{ms} > {text}'
+
+if (isDevelopment) {
+  log.transports.console.level = 'debug'
+  log.transports.file.level = 'info'
+} else {
+  log.transports.console.level = 'info'
+  log.transports.file.level = 'warn'
+}
+
+log.eventLogger.startLogging()
+
+log.initialize()
+
+export default log
