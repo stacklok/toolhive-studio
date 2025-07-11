63feaef1f0751b3686c5d884d24fd611e5f685f6
Giuseppe Scuglia <peppescg@gmail.com>
2025-07-11 20:15:39 +0200

    feat: show info icon for claude code

--- renderer/src/features/clients/components/grid-card-clients.tsx
+++ renderer/src/features/clients/components/grid-card-clients.tsx
@@ -1,7 +1,5 @@
 import type { ClientMcpClientStatus } from '@/common/api/generated'
-import { InfoIcon } from 'lucide-react'
 import { CardClient } from './card-client'
-import { Alert, AlertDescription } from '@/common/components/ui/alert'
 
 export function GridCardClients({
   clients,
@@ -10,14 +8,6 @@ export function GridCardClients({
 }) {
   return (
     <div className="space-y-6">
-      <Alert className="border-none">
-        <InfoIcon />
-        <AlertDescription>
-          The clients may need to be restarted for the new MCP servers to be
-          properly enabled.
-        </AlertDescription>
-      </Alert>
-
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {clients
           .filter((client) => client.installed && client.client_type)
