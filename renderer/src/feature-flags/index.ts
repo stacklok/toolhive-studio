9e48c61ad37aea669befffd742983f5f1325a187
Daniel Kantor <kd@stacklok.com>
2025-07-14 13:24:00 +0200

    allow overriding feature flags

--- renderer/src/feature-flags/index.ts
+++ renderer/src/feature-flags/index.ts
@@ -3,5 +3,8 @@ import features from './toggles.json' assert { type: 'json' }
 type FeatureName = keyof typeof features
 
 export function isFeatureEnabled(feature: FeatureName): boolean {
-  return features[feature] === true
+  return (
+    features[feature] === true ||
+    Boolean(localStorage.getItem(`feature-${feature}`)) === true
+  )
 }
