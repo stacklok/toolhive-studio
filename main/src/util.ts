7876b58a9d3e418961e25404cb3c6e0eff952413
Giuseppe Scuglia <peppescg@gmail.com>
2025-07-15 17:51:40 +0200

    fix: remove unneeded logic

--- main/src/util.ts
+++ main/src/util.ts
@@ -1,35 +1,12 @@
 import { app, BrowserWindow } from 'electron'
-import { execSync } from 'node:child_process'
 import log from './logger'
 import { delay } from '../../utils/delay'
-
-function getVersionFromGit(): string {
-  try {
-    const exactTag = execSync('git describe --exact-match --tags HEAD', {
-      encoding: 'utf8',
-      stdio: 'pipe',
-    }).trim()
-    return exactTag.replace(/^v/, '')
-  } catch {
-    try {
-      const describe = execSync('git describe --tags --always', {
-        encoding: 'utf8',
-        stdio: 'pipe',
-      }).trim()
-
-      const version = describe.replace(/^v/, '').split('-')[0]
-      return version ?? app.getVersion()
-    } catch {
-      return app.getVersion()
-    }
-  }
-}
 
 export function getAppVersion(): string {
   if (process.env.SENTRY_RELEASE) {
     return process.env.SENTRY_RELEASE
   }
-  return getVersionFromGit()
+  return app.getVersion()
 }
 
 export async function pollWindowReady(window: BrowserWindow): Promise<void> {
@@ -46,6 +23,7 @@ export async function pollWindowReady(window: BrowserWindow): Promise<void> {
 export function isOfficialReleaseBuild(): boolean {
   try {
     const version = getAppVersion()
+    console.log('version', version)
     return /^\d+\.\d+\.\d+$/.test(version)
   } catch {
     log.error('Failed to get app version')
