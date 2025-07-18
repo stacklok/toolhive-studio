d03c137d287345dce764315b037c51775a3ba6d6
Daniel Kantor <kd@stacklok.com>
2025-07-18 20:17:27 +0200

    .

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,15 +13,15 @@ import { defineConfig } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './e2e-tests',
-  timeout: 5 * 60 * 1000,
+  timeout: 1 * 60 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
