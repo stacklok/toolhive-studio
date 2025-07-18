88f4db28cb245b6781738ac8c668ebeb21e9811f
Daniel Kantor <kd@stacklok.com>
2025-07-18 16:30:24 +0200

    .

--- e2e-tests/example.spec.ts
+++ e2e-tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, _electron as electron } from '@playwright/test'
 
-test('example test', async () => {
+test('app starts and stops properly', async () => {
   const electronApp = await electron.launch({ args: ['.'] })
   const isPackaged = await electronApp.evaluate(async ({ app }) => {
     return app.isPackaged
@@ -9,7 +9,10 @@ test('example test', async () => {
   expect(isPackaged).toBe(false)
 
   const window = await electronApp.firstWindow()
-  // await window.screenshot({ path: 'intro.png' })
+
+  const header = window.getByRole('heading', { name: 'MCP Servers' })
+  await header.waitFor()
+  expect(header).toBeVisible()
 
   const appToClose = electronApp.close()
   const stoppingMessage = window.getByText('Stopping MCP Servers')
