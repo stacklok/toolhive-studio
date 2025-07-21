6e82e78632a4050208003f49e7a23a4e450420f2
Daniel Kantor <kd@stacklok.com>
2025-07-21 12:54:50 +0200

    add minimally useful test

--- e2e-tests/open-toolhive.spec.ts
+++ e2e-tests/open-toolhive.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from './fixtures/electron'
 
-test('app starts and stops properly', async ({ window }) => {
-  const header = window.getByRole('heading', {
-    name: /add your first mcp server/i,
-  })
-  await header.waitFor()
-  expect(header).toBeVisible()
-})
+// test('app starts and stops properly', async ({ window }) => {
+//   const header = window.getByRole('heading', {
+//     name: /add your first mcp server/i,
+//   })
+//   await header.waitFor()
+//   expect(header).toBeVisible()
+// })
 
-test('install fetch', async ({ window }) => {
+test('install & uninstall fetch', async ({ window }) => {
   await window.getByRole('link', { name: /browse registry/i }).click()
   await window
     .getByRole('button', {
@@ -25,4 +25,30 @@ test('install fetch', async ({ window }) => {
       name: /install server/i,
     })
     .click()
+  await window
+    .getByRole('link', {
+      name: /view/i,
+    })
+    .click()
+  await window.getByText('Running').waitFor()
+  await window
+    .getByRole('button', {
+      name: /more options/i,
+    })
+    .click()
+  await window
+    .getByRole('menuitem', {
+      name: /remove/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /remove/i,
+    })
+    .click()
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
+  })
+  await header.waitFor()
+  expect(header).toBeVisible()
 })
