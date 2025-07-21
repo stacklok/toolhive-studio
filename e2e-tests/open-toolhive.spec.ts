19877082cb73b6506438d572eb041c5f609636c8
Daniel Kantor <kd@stacklok.com>
2025-07-21 10:34:56 +0200

    add test that could actually pass

--- e2e-tests/open-toolhive.spec.ts
+++ e2e-tests/open-toolhive.spec.ts
@@ -1,17 +1,22 @@
-import { test, _electron as electron } from '@playwright/test'
+import { test, expect, _electron as electron } from '@playwright/test'
 
 test('app starts and stops properly', async () => {
-  const electronApp = await electron.launch({
-    args: ['.'],
-    recordVideo: { dir: 'test-videos' },
+  const electronApp = await electron.launch({ args: ['.'] })
+  const isPackaged = await electronApp.evaluate(async ({ app }) => {
+    return app.isPackaged
   })
 
+  expect(isPackaged).toBe(false)
+
   const window = await electronApp.firstWindow()
-  await window.waitForLoadState('networkidle')
-  const mainTitle = window.getByRole('heading', {
-    name: /mcp servers/i,
+
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
   })
-  await mainTitle.isVisible()
+  await header.waitFor()
+  expect(header).toBeVisible()
+
   const appToClose = electronApp.close()
+
   await appToClose
 })
