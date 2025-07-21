f818b0b4a8d2da18d254a6b27a7ffa518cab73ed
Dániel Kántor <kd@stacklok.com>
2025-07-21 15:19:04 +0200

    test: add end-to-end testing (#621)
    * test: add playwright
    
    * test: exclude e2e tests from vitest
    
    * remove projects array from playwright
    
    * properly build app before running playwright
    
    * ci: fix e2e
    
    * fix ci
    
    * add assertion
    
    * .
    
    * run using xvfb-run to compensate for lack of real screen
    
    * .
    
    * test shutdown message
    
    * .
    
    * .
    
    * add ephemeral container
    
    * .
    
    * .
    
    * Revert "."
    
    This reverts commit f9baaaad5136e266ccc68e62d81e02d882fea6ff.
    
    * Revert "."
    
    This reverts commit 9928e6e78fea8dd982fbe5d83bd7bacc9be8aa6e.
    
    * Revert "add ephemeral container"
    
    This reverts commit 6126d9fa43d5057fecb682ac8890f1996aa2507a.
    
    * add testcontainers
    
    * set up docker buildx
    
    * record failure video
    
    * .
    
    * .
    
    * .
    
    * .
    
    * clean up test
    
    * .
    
    * add test that could actually pass
    
    * cleanup
    
    * temporarily disable video recording
    
    * preserve test-results folder ( used for traces )
    
    * .
    
    * record video of test failures
    
    * fix dependencies for e2e
    
    * .
    
    * .
    
    * attempt to fix keyring in github actions
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * extract electron logic to a new fixture
    
    * simplify test
    
    * actually install mcp server
    
    * add minimally useful test
    
    * have at least 2 test cases
    
    * try re-enabling sandboxing
    
    * Revert "try re-enabling sandboxing"
    
    This reverts commit 0da524d5e62720b918bf0f5f47a651e907423efd.
    
    * remove 'master' branch

---
+++ e2e-tests/open-toolhive.spec.ts
@@ -0,0 +1,54 @@
+import { test, expect } from './fixtures/electron'
+
+test('app starts and stops properly', async ({ window }) => {
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
+  })
+  await header.waitFor()
+  expect(header).toBeVisible()
+})
+
+test('install & uninstall fetch', async ({ window }) => {
+  await window.getByRole('link', { name: /browse registry/i }).click()
+  await window
+    .getByRole('button', {
+      name: /fetch/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /install server/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /install server/i,
+    })
+    .click()
+  await window
+    .getByRole('link', {
+      name: /view/i,
+    })
+    .click()
+  await window.getByText('Running').waitFor()
+  await window
+    .getByRole('button', {
+      name: /more options/i,
+    })
+    .click()
+  await window
+    .getByRole('menuitem', {
+      name: /remove/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /remove/i,
+    })
+    .click()
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
+  })
+  await header.waitFor()
+  expect(header).toBeVisible()
+})
---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from '@playwright/test'
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e-tests',
+  timeout: 2 * 60 * 1000,
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    video: 'on',
+    screenshot: 'only-on-failure',
+  },
+})
