916a75759e3bd1f412898d26be54d97490455d5b
Daniel Kantor <kd@stacklok.com>
2025-07-21 12:55:39 +0200

    have at least 2 test cases

--- e2e-tests/open-toolhive.spec.ts
+++ e2e-tests/open-toolhive.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from './fixtures/electron'
 
-// test('app starts and stops properly', async ({ window }) => {
-//   const header = window.getByRole('heading', {
-//     name: /add your first mcp server/i,
-//   })
-//   await header.waitFor()
-//   expect(header).toBeVisible()
-// })
+test('app starts and stops properly', async ({ window }) => {
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
+  })
+  await header.waitFor()
+  expect(header).toBeVisible()
+})
 
 test('install & uninstall fetch', async ({ window }) => {
   await window.getByRole('link', { name: /browse registry/i }).click()
