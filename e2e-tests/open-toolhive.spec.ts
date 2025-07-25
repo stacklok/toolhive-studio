8a6c09ff089e6f68fc60b592f1ca4bae6d064a68
Daniel Kantor <kd@stacklok.com>
2025-07-25 17:30:40 +0200

    test(e2e): add test case for sequentialthinking

--- e2e-tests/open-toolhive.spec.ts
+++ e2e-tests/open-toolhive.spec.ts
@@ -52,3 +52,48 @@ test('install & uninstall fetch', async ({ window }) => {
   await header.waitFor()
   expect(header).toBeVisible()
 })
+
+test('network isolation form', async ({ window }) => {
+  await window.getByRole('link', { name: /browse registry/i }).click()
+  await window
+    .getByRole('button', {
+      name: /sequentialthinking/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /install server/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /install server/i,
+    })
+    .click()
+  await window
+    .getByRole('link', {
+      name: /view/i,
+    })
+    .click()
+  await window.getByText('Running').waitFor()
+  await window
+    .getByRole('button', {
+      name: /more options/i,
+    })
+    .click()
+  await window
+    .getByRole('menuitem', {
+      name: /remove/i,
+    })
+    .click()
+  await window
+    .getByRole('button', {
+      name: /remove/i,
+    })
+    .click()
+  const header = window.getByRole('heading', {
+    name: /add your first mcp server/i,
+  })
+  await header.waitFor()
+  expect(header).toBeVisible()
+})
