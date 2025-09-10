975688cd14ce549ee58f38a82a7719c5e1b4c2e1
Giuseppe Scuglia <peppescg@gmail.com>
2025-09-10 16:09:34 +0200

    fix: empty state mcp servers

--- renderer/src/common/components/empty-state.tsx
+++ renderer/src/common/components/empty-state.tsx
@@ -6,32 +6,32 @@ const actionsStyle = cva('mx-auto mt-8', {
     actions: {
       1: '',
       2: 'grid grid-cols-2 gap-2',
-      3: 'grid grid-cols-3 gap-2',
     },
   },
 })
 
-function Actions({
-  actions,
-}: {
-  actions: [ReactNode, ReactNode?, ReactNode?]
-}) {
+function Actions({ actions }: { actions: [ReactNode, ReactNode?] }) {
   return (
     <div className={actionsStyle({ actions: actions.length })}>{actions}</div>
   )
 }
+
+type EmptyStateProps = {
+  illustration: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
+  body: string
+} & (
+  | { actions: [ReactNode, ReactNode?]; children?: never }
+  | { actions?: never; children: ReactNode }
+)
 
 export function EmptyState({
   actions,
   body,
   illustration: Illustration,
   title,
-}: {
-  illustration: (props: SVGProps<SVGSVGElement>) => JSX.Element
-  title: string
-  body: string
-  actions: [ReactNode, ReactNode?, ReactNode?] | null
-}) {
+  children,
+}: EmptyStateProps) {
   return (
     <div
       className="mx-auto flex max-w-[40rem] flex-col items-center justify-center
@@ -40,7 +40,7 @@ export function EmptyState({
       <Illustration className="mb-4 size-32" />
       <h4 className="text-foreground mb-2 text-3xl font-bold">{title}</h4>
       <p className="text-muted-foreground">{body}</p>
-      {actions ? <Actions actions={actions} /> : null}
+      {actions ? <Actions actions={actions} /> : children}
     </div>
   )
 }
