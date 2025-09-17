3e6eee63e0844c8ea7357dc19886fb677c153719
Daniel Kantor <kd@stacklok.com>
2025-09-17 19:52:29 +0200

    fallback for when feature flag is off

--- renderer/src/features/clients/components/enable-group-button.tsx
+++ renderer/src/features/clients/components/enable-group-button.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/common/components/ui/button'
-import { Power } from 'lucide-react'
+import { Power, Code } from 'lucide-react'
 import { useManageClientsDialog } from '../hooks/use-manage-clients-dialog'
+import { useFeatureFlag } from '@/common/hooks/use-feature-flag'
+import { featureFlagKeys } from '../../../../../utils/feature-flags'
 
 export function EnableGroupButton({
   groupName,
@@ -10,16 +12,32 @@ export function EnableGroupButton({
   className?: string
 }) {
   const { openDialog } = useManageClientsDialog(groupName)
+  const groupsEnabled = useFeatureFlag(featureFlagKeys.GROUPS)
+
+  if (groupsEnabled) {
+    return (
+      <Button
+        variant="enable"
+        onClick={() =>
+          openDialog({ title: 'Enable Group', confirmText: 'Enable' })
+        }
+        className={className}
+      >
+        Enable group
+        <Power className="ml-2 h-4 w-4" />
+      </Button>
+    )
+  }
+
+  // Temporary behavior while feature flag is off
   return (
     <Button
-      variant="enable"
-      onClick={() =>
-        openDialog({ title: 'Enable Group', confirmText: 'Enable' })
-      }
+      variant="outline"
+      onClick={() => openDialog({ title: 'Add a client', confirmText: 'Add' })}
       className={className}
     >
-      Enable group
-      <Power className="h-4 w-4" />
+      <Code className="mr-2 h-4 w-4" />
+      Add a client
     </Button>
   )
 }
