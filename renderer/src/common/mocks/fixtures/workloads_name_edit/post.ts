6b2cd187d2e6d7f218d25a2f18f934824f5c79f3
Samuele Verzi <samu@stacklok.com>
2025-10-09 15:29:40 +0200

    test: more realistic port on fixtures

--- renderer/src/common/mocks/fixtures/workloads_name_edit/post.ts
+++ renderer/src/common/mocks/fixtures/workloads_name_edit/post.ts
@@ -2,5 +2,5 @@ import type { PostApiV1BetaWorkloadsByNameEditResponse } from '@api/types.gen'
 
 export default {
   name: 'commodo',
-  port: -46951877,
+  port: 51877,
 } satisfies PostApiV1BetaWorkloadsByNameEditResponse
