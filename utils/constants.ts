cc245c90c499704ee655b34426a1f3607bae0c55
Giuseppe Scuglia <peppescg@gmail.com>
2025-11-12 10:47:40 +0100

    chore: handle toolhive version through renovate

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,6 @@
+/**
+ * ToolHive CLI version.
+ * This is managed by Renovate and updated automatically when new versions are released.
+ * renovate: datasource=github-releases depName=stacklok/toolhive versioning=semver
+ */
 export const TOOLHIVE_VERSION = process.env.THV_VERSION ?? 'v0.6.2'
