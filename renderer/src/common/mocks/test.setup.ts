391db6aad2b15364361bbea9589991838d17ed86
Daniel Kantor <kd@stacklok.com>
2025-12-10 19:21:35 +0100

    remove unused file

--- renderer/src/common/mocks/test.setup.ts
+++
@@ -1,8 +0,0 @@
-import { afterAll, afterEach, beforeAll, beforeEach } from 'vitest'
-import { resetAllAutoAPIMocks } from './autoAPIMock'
-import { server } from './node'
-
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-beforeEach(() => resetAllAutoAPIMocks())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
