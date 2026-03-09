681a47edd4b1975c54673f382c8a97e66e2ef811
Daniel Kantor <kd@stacklok.com>
2026-03-09 13:16:21 +0100

    mac related fix

--- renderer/src/common/components/layout/top-nav/container.tsx
+++ renderer/src/common/components/layout/top-nav/container.tsx
@@ -3,8 +3,8 @@ import { twMerge } from 'tailwind-merge'
 import { getOsDesignVariant } from '@/common/lib/os-design'
 
 function getPlatformSpecificHeaderClasses() {
-  // Left padding for macOS traffic-light buttons + top offset for title bar
-  return getOsDesignVariant() === 'mac' ? 'pl-26 pt-0.5' : ''
+  // Left padding to clear the macOS traffic-light buttons
+  return getOsDesignVariant() === 'mac' ? 'pl-26' : ''
 }
 
 export function TopNavContainer(props: HTMLProps<HTMLElement>) {
